710caaa2b296d7942efdde4319b3ea1d7036d32b
mscamargo <michelcamargo@outlook.com>
2023-04-07 14:00:27 -0300

    REST API desenvolvida em NestJS com base de dados Postgres
    Changelog:
      * Inicialização do projeto NestJS;
      * Instalação e configuração: Prisma, Docker e Swagger;

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { PrismaService } from './prisma/prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(3030);
+}
+bootstrap();
