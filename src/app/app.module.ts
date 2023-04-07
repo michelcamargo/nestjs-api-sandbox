5b70dab98f2910003d3edd0bc242d68171d62179
mscamargo <michelcamargo@outlook.com>
2023-04-07 15:49:37 -0300

    Introdução aos Middlewares
    Changelog:
      * Adição de middleware exemplo;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ArticlesModule } from '../articles/articles.module';
+import { LoggerMiddleware } from '../common/middleware/logger.middleware';
+import { ArticlesController } from '../articles/articles.controller';
 
 @Module({
   imports: [PrismaModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      // .forRoutes({ path: 'private', method: RequestMethod.GET });
+      // .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+      .exclude(
+        { path: 'public', method: RequestMethod.GET },
+        { path: 'public', method: RequestMethod.POST },
+        'private/(.*)',
+      )
+      .forRoutes(ArticlesController);
+  }
+}
